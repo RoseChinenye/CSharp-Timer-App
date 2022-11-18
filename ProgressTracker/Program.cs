3291be67d3e5f53d699a5fa9f2d549f967ab1a01
Chinenye <c.okeke@genesystechhub.com>
2022-11-18 11:02:17 +0100

    Added more features

--- ProgressTracker/Program.cs
+++ ProgressTracker/Program.cs
@@ -3,62 +3,17 @@ using System.Timers;
 
 namespace ProgressTracker
 {
-    public class Tracker
-    {
-
-
-        private static System.Timers.Timer timer;
-
-        public static void Main()
+    class tracker{
+        static void Main(string[] args)
         {
-            Tracker tracker = new Tracker();
-          
-            Console.Write("Enter work time in minutes : ");
-            int workingTime = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter rest time in minutes : ");
-            int restingTime = Convert.ToInt32(Console.ReadLine());
+            Tracker timer = new Tracker();
 
-            displayWorkTime(workingTime);
-            RestTime(restingTime);
-            Console.ReadLine();
+            timer.StartTracker();
 
-
-            static void displayWorkTime(int workTime)
-            {
-                
-                timer = new System.Timers.Timer(workTime * 60000);
-                Console.WriteLine("Work time started {0:HH:mm:ss}", DateTime.Now, workTime);
-
-                
-                timer.Elapsed += Work;
-                timer.AutoReset = false;
-                timer.Enabled = true;
-            }
-
-            static void RestTime(int restTime)
-            {
-                
-                timer = new System.Timers.Timer(restTime * 60000);
-                Console.WriteLine("Rest time started {0:HH:mm:ss}", DateTime.Now, restTime);
-
-                
-                timer.Elapsed += Rest;
-                timer.AutoReset = false;
-                timer.Enabled = true;
-            }
-
-            static void Work(Object source, ElapsedEventArgs e)
-            {
-                Console.Write("");
-            }
-
-            static void Rest(Object source, ElapsedEventArgs e)
-            {
-                Console.Write("");
-            }
 
         }
+
     }
 
 
--- ProgressTracker/Tracker.cs
+++ ProgressTracker/Tracker.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +10,141 @@ using System.Timers;
 
 namespace ProgressTracker
 {
-    public class Tracker2
+
+    public class Tracker
     {
-        
-        public Tracker2() 
+        public void StartTracker()
         {
-            
+
+            next();
 
         }
 
-        public void displayTracker()
+        public void WorkTime()
         {
-           
+            try {
+
+            DateTime WorkTimeStart = DateTime.Now;
+
+            Console.WriteLine("Time Now: ");
+            Console.WriteLine(WorkTimeStart);
+            Console.WriteLine("Press any key to Continue");
+            Console.ReadKey();
+
+            Console.WriteLine("What do you want to do? ");
+            string? activity = Console.ReadLine();
+
+            Console.WriteLine("\nHow many minutes do you want to {0}: ", activity);
+            double Time = Convert.ToDouble(Console.ReadLine());
+
+            int ConvertToMillis = (int)Millis(Time);
+
+            Thread.Sleep(ConvertToMillis);
+            Console.Beep();
+
+            Console.WriteLine("\nTime for {0} has elasped", activity);
+            DateTime WorkTimeEnd = DateTime.Now;
+            Console.WriteLine("{0} ended by {1} ", activity, WorkTimeEnd);
+
+            TimeSpan TimeLimit = WorkTimeEnd - WorkTimeStart;
+            TimeSpan TimeLimitInMins = (TimeLimit/60);
+
+            Console.WriteLine("The total time spent is: {0} mins", TimeLimitInMins);
+
         }
-        
-       
+        catch (Exception e){
+        Console.WriteLine(e.Message);
+        }
+}
+
+        public double Millis(double minutes)
+        {
+            double MinsToSeconds = (minutes * 60000);
+
+            return MinsToSeconds;
+        }
+
+        public void RestTime()
+        {
+            try { 
+            Console.WriteLine("Time Now:");
+            DateTime TimeRestStart = DateTime.Now;
+            Console.WriteLine(TimeRestStart);
+
+            Console.WriteLine("Press any key to Continue");
+            Console.ReadKey();
+
+            Console.WriteLine("What do you want to do? ");
+            string? activity2 = Console.ReadLine();
+
+            Console.WriteLine("\nHow many minutes do you want to {0}: ", activity2);
+            double Time2 = Convert.ToDouble(Console.ReadLine());
+
+            int ConvertToMillis2 = (int)Millis(Time2);
+
+            Thread.Sleep(ConvertToMillis2);
+            Console.Beep();
+
+            Console.WriteLine("\nTime for {0} has elasped", activity2);
+            DateTime RestTimeEnd = DateTime.Now;
+            Console.WriteLine("{0} ended by {1} ", activity2, RestTimeEnd);
+
+            TimeSpan TimeLimit2 = RestTimeEnd - TimeRestStart;
+
+            TimeSpan TimeLimitInMins2 = (TimeLimit2/60);
+
+            Console.WriteLine("The total time spent is: {0} mins", TimeLimitInMins2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+
+
+        public void next()
+        {
+            try { 
+            int sessions = 0;
+
+            do
+            {
+                Console.WriteLine("This is Progress tracker App!");
+                Console.WriteLine("To get Started, Enter START: ");
+                string? programStart = Console.ReadLine().ToUpper();
+
+                if (programStart == "START")
+                {
+                    WorkTime();
+                    RestTime();
+                    sessions++;
+                    Console.WriteLine($"\nNumber of sessions: {sessions}");
+                }
+                else if (programStart == "END")
+                {
+                    Console.WriteLine("This is the end of this session. Thank You!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Pls enter START or END: ");
+
+                }
+
+            }
+
+            while (true);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    
     }
+            
 }
